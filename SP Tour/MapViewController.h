6f7c6d4e8629a6cc37fadc6eaa240278a4289784
Wei Guang <hweiguang@users.noreply.github.com>
2011-06-29 16:51:55 +0800

    Adding views and 3rd party APIs

---
+++ SP Tour/MapViewController.h
@@ -0,0 +1,22 @@
+//
+//  MapViewController.h
+//  SP Tour
+//
+//  Created by Wei Guang on 6/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ArcGIS.h"
+
+@interface MapViewController : UIViewController <AGSMapViewDelegate> {
+    AGSMapView *_mapView;
+    AGSGraphicsLayer *_graphicsLayer;
+	AGSCalloutTemplate *_CalloutTemplate;
+}
+
+@property (nonatomic, retain) IBOutlet AGSMapView *mapView;
+@property (nonatomic, retain) AGSGraphicsLayer *graphicsLayer;
+@property (nonatomic, retain) AGSCalloutTemplate *CalloutTemplate;
+
+@end
