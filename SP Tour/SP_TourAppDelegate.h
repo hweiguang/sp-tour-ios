36fa53e90747f721d4259a25fe0a5a6393e745ca
weiguang <weiguang1991@gmail.com>
2011-06-29 09:47:51 +0800

    First push

---
+++ SP Tour/SP_TourAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SP_TourAppDelegate.h
+//  SP Tour
+//
+//  Created by Wei Guang on 6/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SP_TourAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
